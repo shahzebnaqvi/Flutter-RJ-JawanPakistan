a63b169fbae3807e44214d62115697f22115f85c
hamzakamelen <hamzakamelen12345@gmail.com>
2024-10-01 22:03:13 +0500

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:containerapp/firebase_options.dart';
 import 'package:containerapp/screens/home_screen.dart';
 import 'package:containerapp/screens/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
