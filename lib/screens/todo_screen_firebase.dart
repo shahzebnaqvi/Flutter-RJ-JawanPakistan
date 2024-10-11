80d744d873ac3daafb79ba459a7677cc821c740d
hamzakamelen <hamzakamelen12345@gmail.com>
2024-10-11 22:03:59 +0500

    crud firebase

--- lib/screens/todo_screen_firebase.dart
+++ lib/screens/todo_screen_firebase.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -15,8 +16,19 @@ class _TodoScreenFirebaseState extends State<TodoScreenFirebase> {
   FirebaseFirestore firestore = FirebaseFirestore.instance;
   List taskList = [];
   Future getTask() async {
-    return await firestore.collection("Task").get();
+    return await firestore
+        .collection("Task")
+        .where('TaskName', isNotEqualTo: "sdsd")
+        .limit(1)
+        .get();
   }
+
+  Stream getTaskStream() async* {
+    yield await firestore.collection("Task").get();
+  }
+
+  final Stream<QuerySnapshot> _taskStream =
+      FirebaseFirestore.instance.collection('Task').snapshots();
 
   @override
   void initState() {
@@ -31,6 +43,11 @@ class _TodoScreenFirebaseState extends State<TodoScreenFirebase> {
       child: Scaffold(
           body: Column(
             children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 0.2,
+                height: MediaQuery.of(context).size.width * 0.2,
+                color: Colors.red,
+              ),
               ElevatedButton(
                   onPressed: () async {
                     var task = await getTask();
@@ -40,69 +57,85 @@ class _TodoScreenFirebaseState extends State<TodoScreenFirebase> {
                     });
                   },
                   child: Icon(Icons.refresh)),
-              FutureBuilder(
-                future: getTask(),
-                builder: (BuildContext context, AsyncSnapshot snapshot) {
-                  // return Text("HEllo");
-                  if (snapshot.hasData) {
-                    return Text("${snapshot.data.docs[0]["TaskName"]}");
-                  } else if (snapshot.connectionState ==
-                      ConnectionState.waiting) {
-                    return Text("waiting");
-                  } else if (snapshot.hasError) {
-                    return Text("Error is here");
-                  } else {
-                    return CircularProgressIndicator();
-                  }
-                },
-              )
-              // Expanded(
-              //   child: ListView.builder(
-              //     itemCount: taskList.length,
-              //     itemBuilder: (context, index) => ListTile(
-              //       title: Text("${taskList[index]["TaskName"]}"),
-              //       leading: Text("abc"),
-              //       trailing: Row(
-              //         mainAxisSize: MainAxisSize.min,
-              //         children: [
-              //           GestureDetector(
-              //               onTap: () async {
-              //                 print("${taskList[index]["TaskName"]}");
-              //                 print("${taskList[index].id}");
-              //                 firestore
-              //                     .collection("Task")
-              //                     .doc("${taskList[index].id}")
-              //                     .update({
-              //                   "TaskName": "YAseen",
-              //                   "Priorty": "High"
-              //                 });
-              //                 var task = await getTask();
-              //                 print(task.docs);
-              //                 setState(() {
-              //                   taskList = task.docs;
-              //                 });
-              //               },
-              //               child: Icon(Icons.edit)),
-              //           GestureDetector(
-              //               onTap: () async {
-              //                 print("${taskList[index]["TaskName"]}");
-              //                 print("${taskList[index].id}");
-              //                 firestore
-              //                     .collection("Task")
-              //                     .doc("${taskList[index].id}")
-              //                     .delete();
-              //                 var task = await getTask();
-              //                 print(task.docs);
-              //                 setState(() {
-              //                   taskList = task.docs;
-              //                 });
-              //               },
-              //               child: Icon(Icons.delete)),
-              //         ],
-              //       ),
-              //     ),
-              //   ),
+              // FutureBuilder(
+              //   future: getTask(),
+              //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+              //     // return Text("HEllo");
+              //     if (snapshot.hasData) {
+              //       return Text("${snapshot.data.docs[0]["TaskName"]}");
+              //     } else if (snapshot.connectionState ==
+              //         ConnectionState.waiting) {
+              //       return Text("waiting");
+              //     } else if (snapshot.hasError) {
+              //       return Text("Error is here");
+              //     } else {
+              //       return CircularProgressIndicator();
+              //     }
+              //   },
               // )
+              // StreamBuilder<QuerySnapshot>(
+              //   stream: _taskStream,
+              //   builder: (BuildContext context, AsyncSnapshot snapshot) {
+              //     // return Text("HEllo");
+              //     if (snapshot.hasData) {
+              //       return Text("${snapshot.data.docs}");
+              //     } else if (snapshot.connectionState ==
+              //         ConnectionState.waiting) {
+              //       return Text("waiting");
+              //     } else if (snapshot.hasError) {
+              //       return Text("Error is here");
+              //     } else {
+              //       return CircularProgressIndicator();
+              //     }
+              //   },
+              // )
+              Expanded(
+                child: ListView.builder(
+                  itemCount: taskList.length,
+                  itemBuilder: (context, index) => ListTile(
+                    title: Text("${taskList[index]["TaskName"]}"),
+                    leading: Text("abc"),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        GestureDetector(
+                            onTap: () async {
+                              print("${taskList[index]["TaskName"]}");
+                              print("${taskList[index].id}");
+                              firestore
+                                  .collection("Task")
+                                  .doc("${taskList[index].id}")
+                                  .update({
+                                "TaskName": "YAseen",
+                                "Priorty": "High"
+                              });
+                              var task = await getTask();
+                              print(task.docs);
+                              setState(() {
+                                taskList = task.docs;
+                              });
+                            },
+                            child: Icon(Icons.edit)),
+                        GestureDetector(
+                            onTap: () async {
+                              print("${taskList[index]["TaskName"]}");
+                              print("${taskList[index].id}");
+                              firestore
+                                  .collection("Task")
+                                  .doc("${taskList[index].id}")
+                                  .delete();
+                              var task = await getTask();
+                              print(task.docs);
+                              setState(() {
+                                taskList = task.docs;
+                              });
+                            },
+                            child: Icon(Icons.delete)),
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
           floatingActionButton: FloatingActionButton(
