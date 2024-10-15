d56ae4c501e8eda2fe571baa357272404e16193b
hamzakamelen <hamzakamelen12345@gmail.com>
2024-10-15 21:46:59 +0500

    pick image from camera and gallery

--- lib/screens/saving_picture_screen.dart
+++ lib/screens/saving_picture_screen.dart
@@ -13,20 +13,57 @@ class SavingPictureScreen extends StatefulWidget {
 }
 
 class _SavingPictureScreenState extends State<SavingPictureScreen> {
-  final storage = FirebaseStorage.instance;
+  final storageRef = FirebaseStorage.instance.ref();
+
   final ImagePicker picker = ImagePicker();
   XFile? image1;
 // Pick an image.
   void pickimageFromCamera() async {
     image1 = await picker.pickImage(source: ImageSource.camera);
-
     setState(() {});
   }
 
   void pickimageFromGallery() async {
     image1 = await picker.pickImage(source: ImageSource.gallery);
-
     setState(() {});
+  }
+
+  uploadToFirebase() async {
+    var file = File(image1!.path);
+    try {
+      var date = DateTime.now();
+      final storageRef = FirebaseStorage.instance.ref();
+      final mountainImagesRef = storageRef.child("images/product_${date}.jpg");
+      await mountainImagesRef.putFile(file);
+    } catch (e) {
+      print("this is error $e");
+    }
+  }
+
+  saveimage(XFile file) async {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    Reference ref = FirebaseStorage.instance
+        .ref()
+        .child('image/some-image${DateTime.now()}.jpg');
+
+    final metadata = SettableMetadata(
+      contentType: 'image/jpeg',
+      customMetadata: {'picked-file-path': file.path},
+    );
+    ref.putData(await file.readAsBytes(), metadata);
+    var imageurl = await ref.getDownloadURL();
+    print("imageurl --$imageurl");
+    firestore
+        .collection("userprofile")
+        .add(
+          {
+            "prodName": "car",
+            "prodImg": "$imageurl",
+            "prodPrice": 33,
+          },
+        )
+        .then((value) => print("Product Added"))
+        .catchError((e) => print("this is error $e"));
   }
 
   @override
@@ -37,6 +74,11 @@ class _SavingPictureScreenState extends State<SavingPictureScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             if (image1 != null) ...[
+              ElevatedButton(
+                  onPressed: () {
+                    uploadToFirebase();
+                  },
+                  child: Text("Upload it To Firebase")),
               Text("${image1!.name}"),
               Image.file(File(image1!.path))
             ],
